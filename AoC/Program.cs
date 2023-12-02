3d1dbd783ad1dcb4e567d311279c18b3ac8b2da6
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-12-02 03:41:16 +0200

    added benchmark.net

--- AoC/Program.cs
+++ AoC/Program.cs
@@ -1,13 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AoC;
+using BenchmarkDotNet.Running;
 
 
-
-
-
-DateTime start = DateTime.Now;
 Quest1 q = new Quest1();
 q.Solve();
-DateTime end = DateTime.Now;
 
-Console.WriteLine(end - start);
+var summary = BenchmarkRunner.Run<BenchmarkQuiz1>();
