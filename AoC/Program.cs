b725c5238af162d64fabea5347e77a1ba796d3ab
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-12-02 03:28:09 +0200

    benchmarking speed like real devs

--- AoC/Program.cs
+++ AoC/Program.cs
@@ -1,8 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AoC;
 
+
+
+
+
+DateTime start = DateTime.Now;
 Quest1 q = new Quest1();
-
-
-
 q.Solve();
+DateTime end = DateTime.Now;
+
+Console.WriteLine(end - start);
