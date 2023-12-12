6df48b5b590bfb377d97665910934e9d3ad6f297
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-12-13 00:18:22 +0200

    quest 12 part 1

--- AoC/Program.cs
+++ AoC/Program.cs
@@ -2,6 +2,7 @@
 using AoC;
 using AoC.Quest10;
 using AoC.Quest11;
+using AoC.Quest12;
 
 
 //BaseQuest q = new Quest1();
@@ -12,8 +13,9 @@ using AoC.Quest11;
 //BaseQuest q = new Quest6();
 //BaseQuest q = new Quest7();
 //BaseQuest q = new Quest8();
-BaseQuest q = new Quest10();
+//BaseQuest q = new Quest10();
 //BaseQuest q = new Quest11();
+BaseQuest q = new Quest12();
 await q.Solve();
 
 //var summary = BenchmarkRunner.Run<BenchmarkQuiz>();
---
+++ AoC/Quest12/Quest12.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.Quest12
+{
+    internal class Quest12 : BaseQuest
+    {
+        public override Task Solve()
+        {
+            string inPath = GetPathTo("quest12_0.in");
+            string outPath = GetPathTo("questResult.out");
+            string[] lines = File.ReadAllLines(inPath);
+            File.WriteAllText(outPath, "");
+            int sum = 0;
+
+            foreach (string line in lines)
+            {
+                string[] splitArray = line.Split(' ');
+                string sequence = splitArray[0];
+                int[] numbers = splitArray[1].Split(',').Select(Int32.Parse).ToArray();
+                Backtracking bkt = new Backtracking(sequence, numbers);
+                int numberOfSolutions = bkt.FindSolutions(sequence, numbers);
+                sum+= numberOfSolutions;
+            }
+
+            Console.WriteLine(sum);
+
+            return Task.CompletedTask;
+        }
+
+        
+    }
+}
